b852d8f977cf48da054bda5ccfbd540567179a58
Leonardo Rothe Tagliafico <leonardo.rothe@shiftactive.com>
2019-05-06 16:09:30 -0500

    Time change

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener('deviceready', () => {
 	cordova.plugins.notification.local.schedule({
 		title: "Cami",
 		text: "¡Recuerda tomar tu pastilla!",
-		every: { minute: 0 },
+		every: { minute: 15 },
 		actions: [
 			{id: "take", title: "Tomar pastilla"},
 			{id: "snooze", title: "Más tarde"}
