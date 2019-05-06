63ace2c4362210bdc27ee2dbedc4f7d4c1cd32cb
Leonardo Rothe Tagliafico <leonardo.rothe@shiftactive.com>
2019-05-06 16:02:10 -0500

    First commit

---
+++ www/js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('deviceready', () => {
+	cordova.plugins.notification.local.schedule({
+		title: "Cami",
+		text: "¡Recuerda tomar tu pastilla!",
+		every: { minute: 0 },
+		actions: [
+			{id: "take", title: "Tomar pastilla"},
+			{id: "snooze", title: "Más tarde"}
+		]
+	});
+	
+	cordova.plugins.notification.local.on("take", () => {
+		alert("Pastilla tomada");
+	});
+
+	cordova.plugins.notification.local.on("snooze", () => {
+		alert("Pastilla pospuesta");
+	});
+});
